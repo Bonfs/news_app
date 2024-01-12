84357a4a7adf6e00bd0406210174b41d9ca3bed5
bonfs <matheusbonfim05@gmail.com>
2024-01-12 16:38:39 -0300

    test for SignInUseCase

--- app/src/test/java/com/bonfs/newsapplication/domain/usecase/SignInUseCaseTest.kt
+++ app/src/test/java/com/bonfs/newsapplication/domain/usecase/SignInUseCaseTest.kt
@@ -1,14 +1,22 @@
 package com.bonfs.newsapplication.domain.usecase
 
+import com.bonfs.newsapplication.news.data.model.UserDTO
+import com.bonfs.newsapplication.news.domain.model.NewsModel
+import com.bonfs.newsapplication.news.domain.model.ResponseResult
 import com.bonfs.newsapplication.news.domain.repository.SessionRepository
 import com.bonfs.newsapplication.news.domain.usecase.SignInUseCase
+import io.mockk.coEvery
 import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 internal class SignInUseCaseTest {
     private lateinit var subject: SignInUseCase
     private val sessionRepository: SessionRepository = mockk()
+    private val user = UserDTO("n", "m")
 
     @Before
     fun setup() {
@@ -16,7 +24,11 @@ internal class SignInUseCaseTest {
     }
 
     @Test
-    fun `when execute correct should return success response`() {
+    fun `when execute correct should return success response`() = runTest {
+        coEvery { subject.execute(any(), any()) } returns ResponseResult.Success(NewsModel(user))
 
+        val response = subject.execute("", "")
+
+        assert(response is ResponseResult.Success)
     }
 }
